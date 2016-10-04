4e9a56496e2c03b654b317465b44d861d4daac1f
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2016-10-04 18:49:25 -0500

    Added some ASCII art
    Signed-off-by: Gigabyte-Giant <brynden.bielefeld@hotmail.com>

--- Include/Kernel.h
+++ Include/Kernel.h
@@ -7,10 +7,25 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "KernelMacros.h"
 #include "IO/VGATextMode/VGATextModeBuffer.h"
 
+#define KERNEL_ENTRY_MESSAGE ( \
+    "******       **     *******  \n" \
+    "*    **     ****    **    ** \n" \
+    "*    **    **  **   **     **\n" \
+    "******    **    **  **     **\n" \
+    "*     ** ********** **     **\n" \
+    "*     ** **      ** **    ** \n" \
+    "*******  **      ** *******  kernel\n" \
+)
+
+// The 'kernel_entry' function serves as the entry point for the (C-side) of
+//  the kernel.
+//
+// We declare this function and state that it should have C linkage.
 C_FUNC void kernel_entry();
 
 #endif
--- Source/Kernel.c
+++ Source/Kernel.c
@@ -8,6 +8,6 @@ void kernel_entry()
 {
     VGATextModeBuffer buffer = vga_textmode_buffer_create();
     
-    vga_textmode_buffer_write_s("Hello, world!\n", VGA_TEXTMODE_COLOR_WHITE,
+    vga_textmode_buffer_write_s(KERNEL_ENTRY_MESSAGE, VGA_TEXTMODE_COLOR_BLUE,
         VGA_TEXTMODE_COLOR_BLACK, &buffer);
 }
